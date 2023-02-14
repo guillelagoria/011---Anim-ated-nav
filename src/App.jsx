f310ae402124fa80770006d5d9dfc08f50856b5e
Guillermo Lagoria <guillermolagoria@gmail.com>
2023-02-14 16:42:47 -0300

    Terminado

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,40 @@
-import { useState } from 'react'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
 
-import React from 'react'
+import React from "react";
 
 export const App = () => {
   const [active, setActive] = useState(true);
 
   const toggleActive = () => {
     setActive(!active);
-  }
+  };
   return (
     <>
-    <body>
-      <nav className={active ? 'active' : ''}>
-        <ul>
-          <li><a >Home</a></li>
-          <li><a >Products</a></li>
-          <li><a >About</a></li>
-          <li><a >Contact</a></li>
-        </ul>
-        <button className='icon' onClick={toggleActive}>
-        <div className='line line1'></div>
-        <div className='line line2'></div>
-        </button>
-      </nav>
-    </body>
+      <body>
+        <nav className={active ? "active" : ""}>
+          <ul>
+            <li>
+              <a>Home</a>
+            </li>
+            <li>
+              <a>Products</a>
+            </li>
+            <li>
+              <a>About</a>
+            </li>
+            <li>
+              <a>Contact</a>
+            </li>
+          </ul>
+          <button className="icon" onClick={toggleActive}>
+            <div className="line line1"></div>
+            <div className="line line2"></div>
+          </button>
+        </nav>
+      </body>
     </>
-    )
-}
+  );
+};
 
-
-
-export default App
+export default App;
